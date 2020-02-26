5490bd638114178392084bd73f132491a60fd65a
0________4 <you@example.com>
2020-02-26 14:03:55 +0300

    Add menu resource to rest of fragments

--- app/src/main/java/com/fortie40/tabexperiment/ui/tabfragment2/TabFragment2.kt
+++ app/src/main/java/com/fortie40/tabexperiment/ui/tabfragment2/TabFragment2.kt
@@ -1,10 +1,8 @@
 package com.fortie40.tabexperiment.ui.tabfragment2
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 
 import com.fortie40.tabexperiment.R
 
@@ -20,6 +18,11 @@ class TabFragment2 : Fragment() {
         setHasOptionsMenu(true)
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_tab2, container, false)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.tab_fragment2_menu, menu)
     }
 
 }
--- app/src/main/java/com/fortie40/tabexperiment/ui/tabfragment3/TabFragment3.kt
+++ app/src/main/java/com/fortie40/tabexperiment/ui/tabfragment3/TabFragment3.kt
@@ -1,10 +1,8 @@
 package com.fortie40.tabexperiment.ui.tabfragment3
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 
 import com.fortie40.tabexperiment.R
 
@@ -20,6 +18,11 @@ class TabFragment3 : Fragment() {
         setHasOptionsMenu(true)
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_tab3, container, false)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.tab_fragment3_menu, menu)
     }
 
 }
